f0f130816671a8f3c7c95b657a29156d5f90b22d
karnan <sooriyakumarkarnan@gmail.com>
2022-04-12 11:25:25 +0530

    configuration chages in config server  and config consumer app

--- Micro-services/06.2-Config-Consumer/src/main/java/com/karnan/rentcloud/configconsumer/controller/ProfileController.java
+++ Micro-services/06.2-Config-Consumer/src/main/java/com/karnan/rentcloud/configconsumer/controller/ProfileController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 import com.karnan.rentcloud.configconsumer.model.MemberProfileConfiguration;
 
@@ -22,4 +22,7 @@ public class ProfileController {
 		return "mprofile";
 
 	}
+//	public MemberProfileConfiguration getConfig() {
+//		return memberProfileConfiguration;
+//	}
 }
--- Micro-services/06.2-Config-Consumer/src/main/java/com/karnan/rentcloud/configconsumer/model/MemberProfileConfiguration.java
+++ Micro-services/06.2-Config-Consumer/src/main/java/com/karnan/rentcloud/configconsumer/model/MemberProfileConfiguration.java
@@ -1,8 +1,10 @@
 package com.karnan.rentcloud.configconsumer.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class MemberProfileConfiguration {
@@ -11,11 +13,12 @@ public class MemberProfileConfiguration {
 	Environment environment;
 	public String getDefaultModel() {
 		System.out.println(environment);
-		System.out.println("");
+		System.out.println(environment.getProperty("vehicle.default.model"));
 		return environment.getProperty("vehicle.default.model");
 	}
 	
 	public String getMinRentPeriod() {
+		System.out.println(environment.getProperty("member.rent.min"));
 		return environment.getProperty("member.rent.min");
 	}
 }
