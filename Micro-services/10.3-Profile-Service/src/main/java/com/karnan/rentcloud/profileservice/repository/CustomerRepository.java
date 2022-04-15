1acc0fcf31b2149673d7a88315ef38b0d402b7c6
karnan <sooriyakumarkarnan@gmail.com>
2022-04-15 12:21:15 +0530

    OAuth with Resource Server

--- Micro-services/10.3-Profile-Service/src/main/java/com/karnan/rentcloud/profileservice/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.karnan.rentcloud.profileservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.karnan.rentcloud.commons.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
-}
