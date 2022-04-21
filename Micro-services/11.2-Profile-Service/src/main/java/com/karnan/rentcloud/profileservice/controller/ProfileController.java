4cee5708d7c7138bf268cceed3ec86d79545e137
karnan131k <sooriyakumarkarnan@gmail.com>
2022-04-21 09:18:26 +0530

    Role Based Access Control

---
+++ Micro-services/11.2-Profile-Service/src/main/java/com/karnan/rentcloud/profileservice/controller/ProfileController.java
@@ -0,0 +1,40 @@
+package com.karnan.rentcloud.profileservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.karnan.rentcloud.commons.model.Customer;
+import com.karnan.rentcloud.profileservice.service.CustomerService;
+
+@RestController
+@RequestMapping(value = "/services")
+public class ProfileController {
+
+	@Autowired
+	CustomerService customerService;
+	
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('create_profile')")
+	public Customer save(@RequestBody Customer customer) {
+		return customerService.save(customer);
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public Customer fetch(@RequestParam int profileId) {
+        return customerService.fetchById(profileId);
+    }
+
+    @RequestMapping(value = "/profiles", method = RequestMethod.GET)
+//    @PreAuthorize("hasAuthority('read_profile')")
+    @PreAuthorize("hasRole('ROLE_operator')")
+    public List<Customer> fetch() {
+        return customerService.fetchAllProfiles();
+    }
+}
